def05a97416db89028a5f489c258faebdc24b419
antoniomartinez <mar20126@byui.edu>
2023-06-18 04:01:13 -0600

    Updated swagger and routes index files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 router.use('/customers', require('./customers'));
 router.use('/pizzas', require('./pizzas'));
-// router.use('/orders', require('./orders'));
 
 module.exports = router;
