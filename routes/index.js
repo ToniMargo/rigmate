b62e8b9953375fc0c2255b8c8e409d8d7eb1c267
antoniomartinez <mar20126@byui.edu>
2023-06-24 12:44:01 -0600

    Completed 2nd semester project

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,43 @@
+const { requiresAuth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
 const router = require('express').Router();
 
-router.use('/', require('./swagger'));
-router.use('/customers', require('./customers'));
-router.use('/pizzas', require('./pizzas'));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// current customer's profile
+router.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// make a new order
+// router.get('/order', requiresAuth(), (req, res) => {
+//     // further coding here
+//   });
+
+// ADMIN ROUTES:
+// database manipulation
+router.use('/', requiresAuth(), require('./swagger'));
+router.use('/customers', requiresAuth(), require('./customers'));
+router.use('/pizzas', requiresAuth(), require('./pizzas'));
+
+// router.use('/', require('./swagger'));
+// router.use('/auth', require('./auth'));
 
 module.exports = router;
